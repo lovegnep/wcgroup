14dd2df5cde077d02100b0d93f5feb2020677c09
lovegnep <lovegnep@163.com>
2018-04-18 23:03:06 +0800

    kkk

--- utils/usercache.js
+++ utils/usercache.js
@@ -1,12 +1,18 @@
 /**
  * Created by Administrator on 2018/4/18.
  */
+const Logger = require('./logger');
+
 let cache = new Map();
 
 function newuser(_id, user){
+    Logger.debug('newuser:',{...cache.keys()});
+    Logger.debug('newuser:',{...cache.values()});
     cache.set(_id,user);
 }
 function getuser(_id){
+    Logger.debug('getuser:',{...cache.keys()});
+    Logger.debug('getuser:',{...cache.values()});
     return cache.get(_id);
 }
 
