4c9847f1dbb9b4f0f8d491dc89de977f9c13f875
lovegnep <lovegnep@163.com>
2018-04-19 21:16:15 +0800

    hehe

--- utils/usercache.js
+++ utils/usercache.js
@@ -9,63 +9,65 @@ const config = require('../config');
 let cache = new Map();
 let client = Redis.createClient(config.redis.port, config.redis.host);
 
-function newuserpro(_id, user){
-	    return new Promise(function(resolve, reject){
-		            client.set(_id, user, function(err, data){
-				                if(err){
-							                reject(err);
-									            }else{
-											                    resolve(data);
-													                }
-						        })
-			        });
+function newuserpro(_id, user) {
+    return new Promise(function (resolve, reject) {
+        client.set(_id, user, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    });
 }
-function getuserpro(_id){
-	    return new Promise(function(resolve, reject){
-		            client.get(_id, function(err, data){
-				                if(err){
-							                reject(err);
-									            }else{
-											                    resolve(data);
-													                }
-						        })
-			        });
+function getuserpro(_id) {
+    return new Promise(function (resolve, reject) {
+        client.get(_id, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    });
 }
 let newuser = async (_id, user) => {
-	    try{
-		            let user = await newuserpro(_id, user);
-			        }catch(err){
-					        Logger.error('newuser: redis set user error:',err);
-						        return null;
-							    }
-	        return user;
+    let user = null;
+    try {
+        user = await newuserpro(_id, user);
+    } catch (err) {
+        Logger.error('newuser: redis set user error:', err);
+        return null;
+    }
+    return user;
 };
 
 let getuser = async (_id) => {
-	    try{
-		            let user = await getuserpro(_id);
-			        }catch(err){
-					        Logger.error('getuser: redis get user error:',err);
-						        return null;
-							    }
-	        return user;
+    let user = null;
+    try {
+        user = await getuserpro(_id);
+    } catch (err) {
+        Logger.error('getuser: redis get user error:', err);
+        return null;
+    }
+    return user;
 }
 
-function newuserold(_id, user){
-	    Logger.debug('newuser:',[...cache.keys()]);
-	        Logger.debug('newuser:',[...cache.values()]);
-		    cache.set(_id,user);
-		        Logger.debug('newuser:',[...cache.keys()]);
-			    Logger.debug('newuser:',[...cache.values()]);
+function newuserold(_id, user) {
+    Logger.debug('newuser:', [...cache.keys()]);
+    Logger.debug('newuser:', [...cache.values()]);
+    cache.set(_id, user);
+    Logger.debug('newuser:', [...cache.keys()]);
+    Logger.debug('newuser:', [...cache.values()]);
 }
-function getuserold(_id){
-	    Logger.debug('getuser:',[...cache.keys()]);
-	        Logger.debug('getuser:',[...cache.values()]);
-		    return cache.get(_id);
+function getuserold(_id) {
+    Logger.debug('getuser:', [...cache.keys()]);
+    Logger.debug('getuser:', [...cache.values()]);
+    return cache.get(_id);
 }
 
 exports = {
-	    newuser:newuser,
-	        getuser:getuser
+    newuser: newuser,
+    getuser: getuser
 };
 Object.assign(module.exports, exports);
