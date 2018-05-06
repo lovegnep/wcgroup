ce3bfe893c1c7bf3e08e37004d0b4c3a7392a9dc
lovegnep <1256136108@qq.com>
2018-05-06 15:43:52 +0800

    ddd

--- controllers/qr.js
+++ controllers/qr.js
@@ -302,12 +302,14 @@ module.exports = {
             Logger.debug('POST /api/viewqr:未浏览过， 收费,qrid:',qrid, userdoc.views);
         }
         if(userdoc.vipid.monthstart && parseInt(Date.now()/1000) - userdoc.vipid.monthstart <= 30*24*3600){//月卡用户
-            await UserInterface.updateViewsAndWeibi(qrid,user._id,true);
+            await UserInterface.updateViewsAndWeibi(qrid,user._id,true);//将qr放入用户已浏览列表中
+            await DataInterface.updateQR({_id:qrid},{$inc:{viewCount:1}});//更新QR浏览量
             return ctx.rest({status:MsgType.EErrorType.EOK});
         }else if(userdoc.weibi < 1){
             return ctx.rest({status:MsgType.EErrorType.ENoWeibi,message:'not enough weibi'});
         }
         let res = await UserInterface.updateViewsAndWeibi(qrid,user._id,false);
+        await DataInterface.updateQR({_id:qrid},{$inc:{viewCount:1}});
         if(res && res._id){
             let qrdoc = await DataInterface.getQR(qrid);
             await UserInterface.newWeibiLog({userid:user._id,source:MsgType.WeiBiSource.EView,change:GmConfig.weibi.view,name:qrdoc.groupname,after:res.weibi});
