bcff292ea18ad87a6e1a4116640e9b5bd935612d
lovegnep <1256136108@qq.com>
2018-05-09 10:12:56 +0800

    ddd

--- models/model.js
+++ models/model.js
@@ -84,6 +84,7 @@ let qrmodel = new mongoose.Schema({
     secret:{type: Boolean, default: false},//是否下架
     f5Time:Date//刷新时间
 });
+qrmodel.index({updateTime:-1});
 let Qrmodel = mongoose.model('Qrmodel', qrmodel);
 
 //评论表
