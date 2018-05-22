a37e2e3348e82d2b97a968ad65386caba0fed9ae
lovegnep <1256136108@qq.com>
2018-05-22 21:04:51 +0800

    ddd

--- test7.js
+++ test7.js
@@ -24,6 +24,7 @@ let f1 = async()=>{
     let count = 0, success=0, fail=0;
     let file = 'group.txt';
     let out = '';
+    let outerr = '';
     for(let i = 0; i < garr.length; i++){
         let res = '';
         try{
@@ -33,8 +34,9 @@ let f1 = async()=>{
             console.log(err,garr[i]);
         }
         count++;
-        if(res === ''){
+        if(!res||res.length < 2){
             fail++;
+            outerr+=garr[i]+'\n'
         }else{
             out+=res;
             success++;
@@ -43,11 +45,13 @@ let f1 = async()=>{
     }
     console.log('garr count:%d, success:%d, fail:%d',count, success, fail);
     Fs.writeFileSync(file, out);
+    Fs.writeFileSync('group_error.txt', outerr);
 }
 let f2 = async()=>{
     let count = 0, success=0, fail=0;
     let file = 'person.txt';
     let out = '';
+    let outerr = '';
     for(let i = 0; i < parr.length; i++){
         let res = '';
         try{
@@ -57,8 +61,9 @@ let f2 = async()=>{
             console.log(err,parr[i]);
         }
         count++;
-        if(res === ''){
+        if(!res||res.length < 2){
             fail++;
+            outerr+=parr[i]+'\n'
         }else{
             out+=res;
             success++;
@@ -66,11 +71,13 @@ let f2 = async()=>{
     }
     console.log('parr count:%d, success:%d, fail:%d',count, success, fail);
     Fs.writeFileSync(file, out);
+    Fs.writeFileSync('person_error.txt', outerr);
 }
 let f3 = async()=>{
     let count = 0, success=0, fail=0;
     let file = 'public.txt';
     let out = '';
+    let outerr = '';
     for(let i = 0; i < oarr.length; i++){
         let res = '';
         try{
@@ -80,8 +87,9 @@ let f3 = async()=>{
             console.log(err,oarr[i]);
         }
         count++;
-        if(res === ''){
+        if(!res||res.length < 2){
             fail++;
+            outerr+=oarr[i]+'\n'
         }else{
             out+=res;
             success++;
@@ -89,6 +97,7 @@ let f3 = async()=>{
     }
     console.log('oarr count:%d, success:%d, fail:%d',count, success, fail);
     Fs.writeFileSync(file, out);
+    Fs.writeFileSync('public_error.txt', outerr);
 }
 f1();
 f2();
